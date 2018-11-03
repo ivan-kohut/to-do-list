c5ed7d27018ee6180c9de16d3993d8ace23a607e
Ivan Kohut <kogut.iwan@gmail.com>
2018-11-03 23:39:20 +0200

    Added migrations.

---
+++ src/Repositories.Concrete/Migrations/20181103210736_InitialSchema.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repositories.Concrete.Migrations
+{
+  public partial class InitialSchema : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.CreateTable(
+          name: "Items",
+          columns: table => new
+          {
+            Id = table.Column<int>(nullable: false)
+                  .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+            Text = table.Column<string>(nullable: false, maxLength: 255)
+          },
+          constraints: table =>
+          {
+            table.PrimaryKey("PK_Items", x => x.Id);
+          });
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropTable(
+          name: "Items");
+    }
+  }
+}
