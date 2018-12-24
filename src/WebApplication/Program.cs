aee75394a30b74041e483775ebe6516ac69b51b5
Ivan Kohut <kogut.iwan@gmail.com>
2018-12-24 19:58:27 +0200

    Simplified main method.

--- src/WebApplication/Program.cs
+++ src/WebApplication/Program.cs
@@ -1,25 +1,16 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 
 namespace WebApplication
 {
   public class Program
   {
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
-      BuildWebHost(args).Run();
-    }
-
-    public static IWebHost BuildWebHost(string[] args) =>
       WebHost.CreateDefaultBuilder(args)
-        .ConfigureAppConfiguration((hostingContext, configurationBuilder) =>
-        {
-          configurationBuilder
-            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-            .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
-        })
         .UseStartup<Startup>()
-        .Build();
+        .Build()
+        .Run();
+    }
   }
 }
