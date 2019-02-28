cfe47d8d88675f247a4ef1cf001044d6778d5a2b
Ivan Kohut <kogut.iwan@gmail.com>
2019-02-28 22:55:12 +0200

    Show users items in admin part

--- src/WebApplication/wwwroot/js/users.js
+++ src/WebApplication/wwwroot/js/users.js
@@ -1,6 +1,7 @@
 ﻿(function (app) {
 
   var usersURL = "/api/v1/users";
+  var usersItems = {};
 
   app.init = function () {
 
@@ -9,10 +10,25 @@
         $("#user-list").append(
           $("<li>")
             .attr("id", data[i].id)
-            .append(`${data[i].name} - ${data[i].email} - ${data[i].isEmailConfirmed} - <a onclick='app.deleteUser(${data[i].id})'>delete</a>`)
+            .append(`${data[i].name} - ${data[i].email} - ${data[i].isEmailConfirmed} - <a onclick='app.showOrHideItems(${data[i].id})'>Show or hide items</a> - <a onclick='app.deleteUser(${data[i].id})'>delete</a>`)
+            .append($("<ul>"))
         );
       }
     });
+  };
+
+  app.showOrHideItems = function (id) {
+
+    var items = usersItems[id];
+
+    if (items === undefined) {
+      callAPI(`${usersURL}/${id}/items`, "GET", null, function (data) {
+        usersItems[id] = data;
+        showOrHideItemsInList(id, data);
+      });
+    } else {
+      showOrHideItemsInList(id, items);
+    }
   };
 
   app.deleteUser = function (id) {
@@ -21,6 +37,21 @@
       $(`#${id}`).remove();
     });
   };
+
+  function showOrHideItemsInList(id, data) {
+
+    var $items = $(`#${id}`).find("ul");
+
+    if ($items.find("li").length === 0) {
+      for (var i = 0; i < data.length; i++) {
+        $items.append(
+          $("<li>").append(data[i].text)
+        );
+      }
+    } else {
+      $items.find("li").remove();
+    }
+  }
 
   function callAPI(url, method, data, callback) {
     $.ajax({
