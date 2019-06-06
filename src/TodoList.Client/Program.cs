f6910156f628ab8faba7d281cc89f3d1ac7f4ddd
Ivan Kohut <kogut.iwan@gmail.com>
2019-06-06 23:13:02 +0300

    Create empty Blazor project

---
+++ src/TodoList.Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace TodoList.Client
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      CreateHostBuilder(args)
+        .Build()
+        .Run();
+    }
+
+    public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args)
+    {
+      return BlazorWebAssemblyHost
+        .CreateDefaultBuilder()
+        .UseBlazorStartup<Startup>();
+    }
+  }
+}
