2dd1e0561d1dfb8dc47d7453dd3f42ed66e14d97
Ivan Kohut <kogut.iwan@gmail.com>
2019-06-26 20:26:24 +0300

    Style "Change password" page

--- src/API.Models/Users/UserChangePasswordModel.cs
+++ src/API.Models/Users/UserChangePasswordModel.cs
@@ -5,12 +5,15 @@ namespace API.Models
   public class UserChangePasswordModel
   {
     [Required]
+    [Display(Name = "Current")]
     public string OldPassword { get; set; }
 
     [Required]
+    [Display(Name = "New")]
     public string NewPassword { get; set; }
 
     [Required]
+    [Display(Name = "Retype new")]
     [Compare(nameof(NewPassword))]
     public string ConfirmNewPassword { get; set; }
   }
