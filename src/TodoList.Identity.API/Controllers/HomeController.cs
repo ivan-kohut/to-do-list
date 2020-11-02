29408d641d6f6584fadeeadef3aaa39758a0c7d2
Ivan Kohut <kogut.iwan@gmail.com>
2020-11-02 23:46:20 +0200

    Prepare action and vm for Error page

---
+++ src/TodoList.Identity.API/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using IdentityServer4.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TodoList.Identity.API.ViewModels;
+
+namespace TodoList.Identity.API.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly IIdentityServerInteractionService interaction;
+
+    public HomeController(IIdentityServerInteractionService interaction)
+    {
+      this.interaction = interaction;
+    }
+
+    public async Task<IActionResult> Error(string errorId) =>
+      View(new ErrorViewModel { Error = await interaction.GetErrorContextAsync(errorId) });
+  }
+}
---
+++ src/TodoList.Identity.API/ViewModels/Home/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿using IdentityServer4.Models;
+
+namespace TodoList.Identity.API.ViewModels
+{
+  public class ErrorViewModel
+  {
+    public ErrorMessage? Error { get; set; }
+  }
+}
