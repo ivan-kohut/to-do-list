db35042004f572d0953ce9eea5799e3ca77bfa63
Ivan Kohut <kogut.iwan@gmail.com>
2021-05-16 16:14:08 +0300

    Validate text of item

--- src/services/Items/TodoList.Items.Domain/Aggregates/ItemAggregate/Item.cs
+++ src/services/Items/TodoList.Items.Domain/Aggregates/ItemAggregate/Item.cs
@@ -1,4 +1,5 @@
-﻿using TodoList.Items.Domain.Shared;
+﻿using System;
+using TodoList.Items.Domain.Shared;
 
 namespace TodoList.Items.Domain.Aggregates.ItemAggregate
 {
@@ -8,14 +9,14 @@ namespace TodoList.Items.Domain.Aggregates.ItemAggregate
     private string _text;
     private int _priority;
 
-    private int _statusId;
     public ItemStatus? Status { get; private set; }
+    private int _statusId;
 
     public Item(int userId, string text, int priority)
     {
       this._userId = userId;
       this._statusId = ItemStatus.Todo.Id;
-      this._text = text;
+      this._text = !string.IsNullOrWhiteSpace(text) ? text : throw new ArgumentNullException(nameof(text));
       this._priority = priority;
     }
   }
