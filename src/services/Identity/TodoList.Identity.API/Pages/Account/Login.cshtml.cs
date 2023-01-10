e2fc3ae0d13fb0a7e90616dc017638874f3f59b0
Ivan Kohut <kogut.iwan@gmail.com>
2023-01-10 18:18:19 +0200

    Add Login page

---
+++ src/services/Identity/TodoList.Identity.API/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,93 @@
+﻿using Duende.IdentityServer.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using TodoList.Identity.API.Data.Entities;
+
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+
+namespace TodoList.Identity.API.Pages.Account
+{
+    public class LoginPageModel : PageModel
+    {
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+        private readonly IIdentityServerInteractionService interaction;
+
+        public LoginPageModel(
+            UserManager<User> userManager,
+            SignInManager<User> signInManager,
+            IIdentityServerInteractionService interaction)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.interaction = interaction;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public IActionResult OnGet(string? returnUrl)
+        {
+            Input = new InputModel { ReturnUrl = returnUrl };
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                User? user = await userManager.FindByEmailAsync(Input.Email!);
+
+                if (user == null)
+                {
+                    // TO DO
+                    ModelState.AddModelError("Input." + nameof(InputModel.Email), "The User is not found.");
+                }
+                else if (!await userManager.CheckPasswordAsync(user, Input.Password!))
+                {
+                    ModelState.AddModelError(nameof(InputModel.Password), "The Password is invalid.");
+                }
+                else if (!user.EmailConfirmed)
+                {
+                    ModelState.AddModelError(nameof(InputModel.Email), "The Email is not confirmed.");
+                }
+                else
+                {
+                    SignInResult signInResult = await signInManager.PasswordSignInAsync(user, Input.Password!, isPersistent: false, lockoutOnFailure: true);
+
+                    if (signInResult.Succeeded)
+                    {
+                        return Redirect(interaction.IsValidReturnUrl(Input.ReturnUrl) ? Input.ReturnUrl! : "/");
+                    }
+                    else if (signInResult.RequiresTwoFactor)
+                    {
+                        // TO DO
+                        return RedirectToPage("LoginWith2fa", new { Input.ReturnUrl });
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    }
+                }
+            }
+
+            return Page();
+        }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string? Email { get; set; }
+
+            [Required]
+            public string? Password { get; set; }
+
+            public string? ReturnUrl { get; set; }
+        }
+    }
+}
--- src/services/Identity/TodoList.Identity.API/Startup.cs
+++ src/services/Identity/TodoList.Identity.API/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
+using System;
 using System.Reflection;
 using TodoList.Identity.API.Data;
 using TodoList.Identity.API.Data.Entities;
@@ -17,110 +18,112 @@ using TodoList.Identity.API.Services;
 
 namespace TodoList.Identity.API
 {
-  public class Startup
-  {
-    private readonly IConfiguration configuration;
-    private readonly IWebHostEnvironment webHostEnvironment;
-
-    public Startup(IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
+    public class Startup
     {
-      this.configuration = configuration;
-      this.webHostEnvironment = webHostEnvironment;
+        private readonly IConfiguration configuration;
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
+        {
+            this.configuration = configuration;
+            this.webHostEnvironment = webHostEnvironment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddRazorPages();
+
+            string connectionString = configuration.GetConnectionString("DefaultConnection")
+                ?? throw new Exception("Connection string is null");
+
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+            services
+                .AddIdentity<User, Role>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            string? migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            services
+                .AddIdentityServer()
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = b => b.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = b => b.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                .AddAspNetIdentity<User>();
+
+            services.AddAuthentication()
+                .AddFacebook(o =>
+                {
+                    o.AppId = configuration["Facebook:AppId"] ?? throw new Exception("Facebook Id is null");
+                    o.AppSecret = configuration["Facebook:AppSecret"] ?? throw new Exception("Facebook Secret is null");
+                    o.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                })
+                .AddGoogle(o =>
+                {
+                    o.ClientId = configuration["Google:ClientId"] ?? throw new Exception("Google Id is null");
+                    o.ClientSecret = configuration["Google:ClientSecret"] ?? throw new Exception("Google Secret is null");
+                    o.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                })
+                .AddGitHub(o =>
+                {
+                    o.ClientId = configuration["Github:ClientId"] ?? throw new Exception("Github Id is null");
+                    o.ClientSecret = configuration["Github:ClientSecret"] ?? throw new Exception("Github Secret is null");
+                    o.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                });
+
+            services.Configure<SendGridOptions>(configuration.GetSection("SendGrid"));
+            services.Configure<EventBusOptions>(configuration.GetSection("EventBus"));
+
+            services.AddSingleton<IEmailService, EmailService>();
+            services.AddSingleton<IEventBusService, RabbitMQEventBusService>();
+
+            services.AddHealthChecks()
+                .AddCheck("self", () => HealthCheckResult.Healthy())
+                .AddSqlServer(connectionString, name: "db")
+                .AddRabbitMQ("amqp://" + configuration["EventBus:Connection"], name: "rabbitmq");
+
+            if (webHostEnvironment.IsDevelopment())
+            {
+                services
+                    .AddMiniProfiler(o => o.RouteBasePath = "/profiler")
+                    .AddEntityFramework();
+            }
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseHttpsRedirection();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseMiniProfiler();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseIdentityServer();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(name: "Delete user", pattern: "admin/users/delete/{id}", defaults: new { controller = "Admin", action = "DeleteUser" });
+                endpoints.MapDefaultControllerRoute();
+                endpoints.MapHealthChecks("/health", new HealthCheckOptions
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+            });
+        }
     }
-
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services
-        .AddControllersWithViews();
-
-      string connectionString = configuration.GetConnectionString("DefaultConnection");
-
-      services
-        .AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
-
-      services
-        .AddIdentity<User, Role>()
-        .AddEntityFrameworkStores<AppDbContext>()
-        .AddDefaultTokenProviders();
-
-      string? migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
-
-      services
-        .AddIdentityServer()
-        .AddConfigurationStore(options =>
-        {
-          options.ConfigureDbContext = b => b.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
-        })
-        .AddOperationalStore(options =>
-        {
-          options.ConfigureDbContext = b => b.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
-        })
-        .AddAspNetIdentity<User>();
-
-      services.AddAuthentication()
-        .AddFacebook(o =>
-        {
-          o.AppId = configuration["Facebook:AppId"];
-          o.AppSecret = configuration["Facebook:AppSecret"];
-          o.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-        })
-        .AddGoogle(o =>
-        {
-          o.ClientId = configuration["Google:ClientId"];
-          o.ClientSecret = configuration["Google:ClientSecret"];
-          o.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-        })
-        .AddGitHub(o =>
-        {
-          o.ClientId = configuration["Github:ClientId"];
-          o.ClientSecret = configuration["Github:ClientSecret"];
-          o.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-        });
-
-      services.Configure<SendGridOptions>(configuration.GetSection("SendGrid"));
-      services.Configure<EventBusOptions>(configuration.GetSection("EventBus"));
-
-      services.AddSingleton<IEmailService, EmailService>();
-      services.AddSingleton<IEventBusService, RabbitMQEventBusService>();
-
-      services.AddHealthChecks()
-        .AddCheck("self", () => HealthCheckResult.Healthy())
-        .AddSqlServer(connectionString, name: "db")
-        .AddRabbitMQ("amqp://" + configuration["EventBus:Connection"], name: "rabbitmq");
-
-      if (webHostEnvironment.IsDevelopment())
-      {
-        services
-          .AddMiniProfiler(o => o.RouteBasePath = "/profiler")
-          .AddEntityFramework();
-      }
-    }
-
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      app.UseHttpsRedirection();
-
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-        app.UseMiniProfiler();
-      }
-
-      app.UseStaticFiles();
-
-      app.UseRouting();
-      app.UseIdentityServer();
-      app.UseAuthorization();
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllerRoute(name: "Delete user", pattern: "admin/users/delete/{id}", defaults: new { controller = "Admin", action = "DeleteUser" });
-        endpoints.MapDefaultControllerRoute();
-        endpoints.MapHealthChecks("/health", new HealthCheckOptions
-        {
-          Predicate = _ => true,
-          ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-        });
-      });
-    }
-  }
 }
