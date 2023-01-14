cda7f216d33e6b53b771952d2424136a5cf38dd4
Ivan Kohut <kogut.iwan@gmail.com>
2023-01-14 16:41:44 +0200

    Add LoginWith2fa page

---
+++ src/services/Identity/TodoList.Identity.API/Pages/Account/LoginWith2fa.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using Duende.IdentityServer.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using TodoList.Identity.API.Data.Entities;
+
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+
+namespace TodoList.Identity.API.Pages.Account
+{
+    [Authorize(AuthenticationSchemes = "Identity.TwoFactorUserId")]
+    public class LoginWith2faPageModel : PageModel
+    {
+        private readonly SignInManager<User> signInManager;
+        private readonly IIdentityServerInteractionService interaction;
+
+        public LoginWith2faPageModel(
+            SignInManager<User> signInManager,
+            IIdentityServerInteractionService interaction)
+        {
+            this.signInManager = signInManager;
+            this.interaction = interaction;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; } = null!;
+
+        public async Task<IActionResult> OnPostAsync(string? returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                SignInResult signInResult = await signInManager.TwoFactorAuthenticatorSignInAsync(
+                    Input.TwoFactorToken!,
+                    isPersistent: false,
+                    rememberClient: false);
+
+                if (signInResult.Succeeded)
+                {
+                    return Redirect(interaction.IsValidReturnUrl(returnUrl) ? returnUrl! : "/");
+                }
+
+                ModelState.AddModelError($"{nameof(Input)}.{nameof(Input.TwoFactorToken)}", "Invalid authenticator code.");
+            }
+
+            return Page();
+        }
+
+        public class InputModel
+        {
+            [Required]
+            [RegularExpression("[0-9]{6}")]
+            public string? TwoFactorToken { get; set; }
+        }
+    }
+}
