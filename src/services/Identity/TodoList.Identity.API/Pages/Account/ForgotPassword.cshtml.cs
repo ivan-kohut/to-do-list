afaf2b4175a27c36bb171f757d97cbc89d624213
Ivan Kohut <kogut.iwan@gmail.com>
2023-01-14 17:43:52 +0200

    Add ForgotPassword page

---
+++ src/services/Identity/TodoList.Identity.API/Pages/Account/ForgotPassword.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using TodoList.Identity.API.Data.Entities;
+using TodoList.Identity.API.Services;
+
+namespace TodoList.Identity.API.Pages.Account
+{
+    public class ForgotPasswordPageModel : PageModel
+    {
+        private readonly UserManager<User> userManager;
+        private readonly IEmailService emailService;
+
+        public ForgotPasswordPageModel(UserManager<User> userManager, IEmailService emailService)
+        {
+            this.userManager = userManager;
+            this.emailService = emailService;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; } = null!;
+
+        public async Task<IActionResult> OnPostAsync(string? returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                User? user = await userManager.FindByEmailAsync(Input.Email!);
+
+                if (user != null && user.EmailConfirmed)
+                {
+                    await emailService.SendEmailAsync(user.Email!, "Reset password", await GeneratePasswordResetMessageAsync(user, returnUrl));
+                }
+
+                return RedirectToPage("ForgotPasswordSuccess");
+            }
+
+            return Page();
+        }
+
+        private async Task<string> GeneratePasswordResetMessageAsync(User user, string? returnUrl)
+        {
+            string callbackUrl = Url.Page(
+                "ResetPassword",
+                pageHandler: null,
+                values: new { id = user.Id, code = await userManager.GeneratePasswordResetTokenAsync(user), returnUrl },
+                protocol: Request.Scheme)!;
+
+            return $"Please reset your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.";
+        }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string? Email { get; set; }
+        }
+    }
+}
