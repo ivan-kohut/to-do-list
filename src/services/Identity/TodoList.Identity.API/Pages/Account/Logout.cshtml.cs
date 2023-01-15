3ec50e788f1b40e3e07f1798b7843d6866c0fa80
Ivan Kohut <kogut.iwan@gmail.com>
2023-01-15 17:30:40 +0200

    Add Logout page

---
+++ src/services/Identity/TodoList.Identity.API/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Duende.IdentityServer.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+using TodoList.Identity.API.Data.Entities;
+
+namespace TodoList.Identity.API.Pages.Account
+{
+    public class LogoutPageModel : PageModel
+    {
+        private readonly SignInManager<User> signInManager;
+        private readonly IIdentityServerInteractionService interaction;
+
+        public LogoutPageModel(SignInManager<User> signInManager, IIdentityServerInteractionService interaction)
+        {
+            this.signInManager = signInManager;
+            this.interaction = interaction;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string? logoutId)
+        {
+            if (User.Identity != null && User.Identity.IsAuthenticated)
+            {
+                await signInManager.SignOutAsync();
+            }
+
+            return Redirect((await interaction.GetLogoutContextAsync(logoutId)).PostLogoutRedirectUri ?? "/");
+        }
+    }
+}
