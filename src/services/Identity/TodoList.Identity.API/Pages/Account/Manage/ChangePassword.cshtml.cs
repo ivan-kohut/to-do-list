58e7bbb22331c1b286af3190eb9270c87a6d6826
Ivan Kohut <kogut.iwan@gmail.com>
2023-01-17 17:37:49 +0200

    Add ChangePassword page

---
+++ src/services/Identity/TodoList.Identity.API/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using TodoList.Identity.API.Data.Entities;
+
+namespace TodoList.Identity.API.Pages.Account.Manage
+{
+    [Authorize(Roles = "user")]
+    public class ChangePasswordPageModel : PageModel
+    {
+        private readonly UserManager<User> userManager;
+
+        public ChangePasswordPageModel(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; } = null!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                User? user = await userManager.GetUserAsync(User);
+
+                if (user is null)
+                {
+                    return NotFound();
+                }
+
+                IdentityResult changePasswordResult = await userManager.ChangePasswordAsync(user, Input.OldPassword!, Input.NewPassword!);
+
+                if (changePasswordResult.Succeeded)
+                {
+                    return RedirectToPage("Index");
+                }
+
+                foreach (IdentityError error in changePasswordResult.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return Page();
+        }
+
+        public class InputModel
+        {
+            [Required]
+            public string? OldPassword { get; set; }
+
+            [Required]
+            public string? NewPassword { get; set; }
+
+            [Required]
+            [Compare(nameof(NewPassword))]
+            public string? ConfirmNewPassword { get; set; }
+        }
+    }
+}
