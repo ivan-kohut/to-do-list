06895e427515f06e3c7c3c6658a5396b8a3bc02c
Ivan Kohut <kogut.iwan@gmail.com>
2023-01-18 14:04:29 +0200

    Add 2FA pages

---
+++ src/services/Identity/TodoList.Identity.API/Pages/Account/Manage/Disable2fa.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+using TodoList.Identity.API.Data.Entities;
+
+namespace TodoList.Identity.API.Pages.Account.Manage
+{
+    [Authorize(Roles = "user")]
+    public class Disable2faPageModel : PageModel
+    {
+        private readonly UserManager<User> userManager;
+
+        public Disable2faPageModel(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            User? user = await userManager.GetUserAsync(User);
+
+            if (user is null)
+            {
+                return NotFound();
+            }
+            else if (!user.TwoFactorEnabled)
+            {
+                return RedirectToPage("Index");
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            User? user = await userManager.GetUserAsync(User);
+
+            if (user is null)
+            {
+                return NotFound();
+            }
+            else if (user.TwoFactorEnabled)
+            {
+                await userManager.SetTwoFactorEnabledAsync(user, enabled: false);
+                await userManager.ResetAuthenticatorKeyAsync(user);
+            }
+
+            return RedirectToPage("Index");
+        }
+    }
+}
